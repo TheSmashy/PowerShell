09426d5d37f9ccfd9df4be4781379add48c9b8b0
TheSmashy <25392589+TheSmashy@users.noreply.github.com>
2021-02-27 12:58:57 -0600

    Add files via upload

---
+++ Test-MemoryUsage.ps1
@@ -0,0 +1,23 @@
+Function Test-MemoryUsage {
+[cmdletbinding()]
+Param()
+ 
+$os = Get-Ciminstance Win32_OperatingSystem
+$pctFree = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
+ 
+if ($pctFree -ge 45) {
+$Status = "OK"
+}
+elseif ($pctFree -ge 15 ) {
+$Status = "Warning"
+}
+else {
+$Status = "Critical"
+}
+ 
+$os | Select @{Name = "Status";Expression = {$Status}},
+@{Name = "PctFree"; Expression = {$pctFree}},
+@{Name = "FreeGB";Expression = {[math]::Round($_.FreePhysicalMemory/1mb,2)}},
+@{Name = "TotalGB";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}
+ 
+}
